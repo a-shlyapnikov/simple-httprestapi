abe9176044523b8d0f5c7173d761de3b6e9274ca
Alexandr <shlyapnikoff2012@gmail.com>
2024-10-07 13:23:36 +0500

    post handler added

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+var message string
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", HelloHandlerPOST).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
 
+func HelloHandlerPOST(w http.ResponseWriter, r *http.Request) {
+	var reqBody requestBody
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	message = reqBody.Message
+	fmt.Fprintf(w, "Hello %s", message)
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello %s", message)
 }
