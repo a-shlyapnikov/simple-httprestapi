71a325a00db71d991eede984b4c0400d781fbd94
Alexandr <shlyapnikoff2012@gmail.com>
2024-10-08 18:56:35 +0500

    add integration with postgres

--- main.go
+++ main.go
@@ -15,10 +15,41 @@ type requestBody struct {
 var message string
 
 func main() {
+	InitDb()
+
+	DB.AutoMigrate(&Message{})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
 	router.HandleFunc("/api/hello", HelloHandlerPOST).Methods("POST")
+	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
+	router.HandleFunc("/api/messages", GetMessage).Methods("GET")
+
 	http.ListenAndServe(":8080", router)
+}
+
+func CreateMessage(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if result := DB.Create(&msg); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(msg)
+}
+
+func GetMessage(w http.ResponseWriter, r *http.Request) {
+	var messages []Message
+	if result := DB.Find(&messages); result.Error != nil {
+		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(messages)
+
 }
 
 func HelloHandlerPOST(w http.ResponseWriter, r *http.Request) {
