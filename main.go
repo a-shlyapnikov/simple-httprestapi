259710310a8c375aad009eb7e0ef509d37d563f8
Alexandr <shlyapnikoff2012@gmail.com>
2024-10-09 11:22:49 +0500

    crud made

--- main.go
+++ main.go
@@ -24,8 +24,48 @@ func main() {
 	router.HandleFunc("/api/hello", HelloHandlerPOST).Methods("POST")
 	router.HandleFunc("/api/messages", CreateMessage).Methods("POST")
 	router.HandleFunc("/api/messages", GetMessage).Methods("GET")
+	router.HandleFunc("/api/messages/{id}", UpdateMessage).Methods("PATCH")
+	router.HandleFunc("/api/messages/{id}", DeleteMessage).Methods("DELETE")
 
 	http.ListenAndServe(":8080", router)
+}
+
+func DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if result := DB.Delete(&msg, id); result.RowsAffected == 0 {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	var msg Message
+	var existingMessage Message
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if result := DB.First(&existingMessage, id); result.Error != nil || result.RowsAffected == 0 {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	if err := DB.Model(&existingMessage).Updates(msg).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&msg)
 }
 
 func CreateMessage(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +79,7 @@ func CreateMessage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(msg)
+	json.NewEncoder(w).Encode(&msg)
 }
 
 func GetMessage(w http.ResponseWriter, r *http.Request) {
